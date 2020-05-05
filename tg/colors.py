0ce4dc76d6888ddf9c317ca4aadf7270635e1ec7
Paul Nameless <reacsdas@gmail.com>
2020-05-05 14:32:04 +0800

    Fix adding wrong colors module

---
+++ tg/colors.py
@@ -0,0 +1,52 @@
+import curses
+
+DEFAULT_FG = curses.COLOR_WHITE
+DEFAULT_BG = curses.COLOR_BLACK
+COLOR_PAIRS = {10: 0}
+
+# colors
+black = curses.COLOR_BLACK
+blue = curses.COLOR_BLUE
+cyan = curses.COLOR_CYAN
+green = curses.COLOR_GREEN
+magenta = curses.COLOR_MAGENTA
+red = curses.COLOR_RED
+white = curses.COLOR_WHITE
+yellow = curses.COLOR_YELLOW
+default = -1
+
+# modes
+normal = curses.A_NORMAL
+bold = curses.A_BOLD
+blink = curses.A_BLINK
+reverse = curses.A_REVERSE
+underline = curses.A_UNDERLINE
+invisible = curses.A_INVIS
+dim = curses.A_DIM
+
+
+def get_color(fg, bg):
+    """Returns the curses color pair for the given fg/bg combination."""
+
+    key = (fg, bg)
+    if key not in COLOR_PAIRS:
+        size = len(COLOR_PAIRS)
+        try:
+            curses.init_pair(size, fg, bg)
+        except curses.error:
+            # If curses.use_default_colors() failed during the initialization
+            # of curses, then using -1 as fg or bg will fail as well, which
+            # we need to handle with fallback-defaults:
+            if fg == -1:  # -1 is the "default" color
+                fg = DEFAULT_FG
+            if bg == -1:  # -1 is the "default" color
+                bg = DEFAULT_BG
+
+            try:
+                curses.init_pair(size, fg, bg)
+            except curses.error:
+                # If this fails too, colors are probably not supported
+                pass
+        COLOR_PAIRS[key] = size
+
+    return curses.color_pair(COLOR_PAIRS[key])
