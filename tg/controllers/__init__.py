f0ddbb2dbbb4101f7eee3948817593e14f922b32
Paul Nameless <reacsdas@gmail.com>
2020-05-19 14:52:58 +0800

    Add fallback to vim if $EDITOR env var is not set

--- tg/controllers/__init__.py
+++ tg/controllers/__init__.py
@@ -121,7 +121,7 @@ class Controller:
     def write_long_msg(self):
         file_path = "/tmp/tg-msg.txt"
         with suspend(self.view) as s:
-            s.call(f"$EDITOR {file_path}")
+            s.call(f"$EDITOR {file_path} || vim {file_path}")
         if not os.path.isfile(file_path):
             return
         with open(file_path) as f:
