30b357da8ff62df08338915b656bb782d383229f
Paul Nameless <reacsdas@gmail.com>
2020-07-14 15:11:05 +0800

    Add docstring to show in help

--- tg/controllers.py
+++ tg/controllers.py
@@ -311,6 +311,7 @@ class Controller:
 
     @bind(msg_handler, ["S"])
     def choose_and_send_file(self) -> None:
+        """Call file picker and send chosen file"""
         chat_id = self.model.chats.id_by_index(self.model.current_chat)
         file_path = None
         if not chat_id:
