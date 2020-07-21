de4c97f7bc9e4a32fba309a93c9d1ebf1e210e6a
lightme16 <sanyazveruk@gmail.com>
2020-07-22 00:49:10 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+import tg
+
+with open("readme.md", "r") as fh:
+    readme = fh.read()
+
+
+setup(
+    long_description=readme,
+    name="tg",
+    version=tg.__version__,
+    description="Terminal client for telegram",
+    url="https://github.com/paul-nameless/tg",
+    author="Paul Nameless",
+    author_email="reacsdas@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["tg"],
+    entry_points={"console_scripts": ["tg = tg.__main__:main"]},
+    python_requires=">=3.8",
+    install_requires=["python-telegram==0.12.0"],
+)
