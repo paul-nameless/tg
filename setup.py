1b8b87358108a8765c2b60ea45c4fc6edc2a4d8e
Denis Gordeev <gordeev-di@ranepa.ru>
2022-04-05 13:07:18 +0300

    updated python-telegram version to 0.15.0

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
     packages=["tg"],
     entry_points={"console_scripts": ["tg = tg.__main__:main"]},
     python_requires=">=3.8",
-    install_requires=["python-telegram==0.14.0"],
+    install_requires=["python-telegram==0.15.0"],
 )
