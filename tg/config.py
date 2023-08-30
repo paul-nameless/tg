6e58d6791c87e59224d78dbde0d0dd6c73ed37f2
c4llv07e <kseandi@gmail.com>
2023-08-30 09:30:26 +0300

    Use XDG standard by default
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- tg/config.py
+++ tg/config.py
@@ -11,14 +11,16 @@ _os_name = platform.system()
 _darwin = "Darwin"
 _linux = "Linux"
 
+def expand_path(path):
+    return os.path.expandvars(os.path.expanduser(path))
 
-CONFIG_DIR = os.path.expanduser("~/.config/tg/")
+CONFIG_DIR = expand_path("$XDG_CONFIG_HOME/tg/")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "conf.py")
-FILES_DIR = os.path.expanduser("~/.cache/tg/")
+FILES_DIR = expand_path("$XDG_CACHE_HOME/tg/")
 MAILCAP_FILE: Optional[str] = None
 
 LOG_LEVEL = "INFO"
-LOG_PATH = os.path.expanduser("~/.local/share/tg/")
+LOG_PATH = expand_path("$XDG_DATA_HOME/tg/")
 
 API_ID = "559815"
 API_HASH = "fd121358f59d764c57c55871aa0807ca"
@@ -78,7 +80,7 @@ KEEP_MEDIA = 7
 
 FILE_PICKER_CMD = "ranger --choosefile={file_path}"
 
-DOWNLOAD_DIR = os.path.expanduser("~/Downloads/")
+DOWNLOAD_DIR = expand_path("$XDG_DOWNLOAD_DIR")
 
 if os.path.isfile(CONFIG_FILE):
     config_params = runpy.run_path(CONFIG_FILE)  # type: ignore
